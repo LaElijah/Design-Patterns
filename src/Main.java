e5d6c22f328f93660afb08e13c74067ab04d4a37
LaElijah <114874165+LaElijah@users.noreply.github.com>
2023-10-01 09:53:19 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+
+
+
+public class Main {
+    public static void main(String[] args)  {
+        System.out.println("Hello, World!");
+
+        User user = new User("Elijah");
+
+        System.out.println(user.name);
+        user.sayHello();
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,18 @@
+public class User {
+
+    // Fields (attributes)
+
+    public String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+
+    public void sayHello() {
+        System.out.println("Hello, my name is " + name);
+    }
+
+
+    
+}
